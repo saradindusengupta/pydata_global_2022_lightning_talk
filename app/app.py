46b72cd7b7254863b37ff1008fd3387754eae449
Saradindu Sengupta <saradindu.mi1@iiitmk.ac.in>
2022-11-28 13:26:52 +0530

    added correction factor dashboard

--- app/app.py
+++ app/app.py
@@ -7,37 +7,36 @@ from random import sample
 import pandas as pd
 import streamlit as st
 import warnings
-import os
 warnings.filterwarnings("ignore")
 
 
 
-st.set_page_config(page_title="Validation", page_icon=None, layout="wide", initial_sidebar_state="auto", menu_items=None)
 
-def build_graph(value):
-    # seed random number generator
+st.set_page_config(page_title="Bessel's Correction", page_icon=None, layout="wide", initial_sidebar_state="auto", menu_items=None)
+
+def bessel_correction(sample_size, pop_size):
+    # Generate population within a range of 0<x<=100000
     seed(1)
-    # generate some integers
-    values = list(randint(0, 100000, 50000))
-    # Declare empty lists to store Variance of the Sample and Correction Factor
+    values = list(randint(0, 100000, pop_size))
+
     var = [0] * 51
     corr = [0] * 51
     n = 0
-    x = 0.1
 
-    # 300 is the number of times a sample of size 'value' is drawn from the population
+    # epoch is 300
     while n < 300:
-        subset = sample(values, int(value))
+        subset = sample(values, int(sample_size))
         sample_mean = np.mean(subset)
         squared_sum = 0
-        for i in range(int(value)):
+        for i in range(int(sample_size)):
             squared_sum = squared_sum + (subset[i] - sample_mean) ** 2
         j = 0
-        x = -3  # Initialize Correction factor as -3
+        # Initialize Correction factor as -3
+        x = -3  
         
-        # Iteratively loop through a number of Correction Factors from -3 to 2
+        # Iteratively loop through a number of Correction Factors from -3 to 3
         while x < 2.1:  
-            var[j] += squared_sum / (value + x)
+            var[j] += squared_sum / (sample_size + x)
             corr[j] = x
             x += 0.1
             j += 1
@@ -47,20 +46,20 @@ def build_graph(value):
     # Calculate Population Mean
     pop_mean = np.mean(values)
     squared = 0
-    for i in range(50000):
+    for i in range(pop_size):
         squared = squared + (values[i] - pop_mean) ** 2
     # Calculate Population Standard Deviation
-    sd = sqrt(squared / 50000)
+    sd = sqrt(squared / pop_size)
 
-    # Store results in a dataframest.slider('SoH threshold', 0.60,0.90,0.80)
-    info = pd.DataFrame(columns=['Correction_Factor', 'Variance', 'SD'])
+    
+    info = pd.DataFrame(columns=['Correction_Factor', 'Variance', 'STD'])
     info['Correction_Factor'] = corr
     info['Variance'] = var
-    info['SD'] = var_calc
+    info['STD'] = var_calc
 
-    table_output = pd.DataFrame(columns=['Sample_Size', 'Dev@0', 'Dev@-1'])
-    table_output.loc[0] = [int(value), abs(sd - info.loc[31, 'SD']) * 100 / sd, 
-                                  abs(sd - info.loc[21, 'SD']) * 100 / sd]
+    table_output = pd.DataFrame(columns=['Sample_Size', 'True_Population_vs_Uncorrected_Standard_deviation', 'True_Population_vs_Unbiased_Standard_deviation'])
+    table_output.loc[0] = [int(sample_size), abs(sd - info.loc[31, 'STD']) * 100 / sd, 
+                                  abs(sd - info.loc[21, 'STD']) * 100 / sd]
 
     data = [dict(table_output.loc[0, ])]
     columns = [
@@ -68,18 +67,36 @@ def build_graph(value):
         for k in list(table_output.columns)
     ]
 
-    fig = px.line(info, x="Correction_Factor", y="SD")
+    fig = px.line(info, x="Correction_Factor", y="STD")
 
-    fig.update_layout(yaxis={'title': 'Mean Standard Deviation'},
-                      title={'text': 'Bessel Correction'}, shapes=[dict(
+    fig.update_layout(yaxis={'title': "Mean Standard Deviation"},
+                      xaxis={'title': "Correction Factor"},
+                      shapes=[dict(
             type='line',
             yref='y', y0=sd, y1=sd,
-            xref='x', x0=-3, x1=2
-        )])
+            xref='x', x0=-3, x1=3)],
+            margin=dict(l=20, r=20, t=20, b=60),
+            paper_bgcolor="LightSteelBlue"
+                      )
+
+    # add annotation
+    fig.add_annotation(dict(font=dict(color='black',size=13),
+                                            x=0,
+                                            y=-0.17,
+                                            showarrow=False,
+                                            text="As the sample size increases, the unbiased standard deviation is closer to uncorrected standard deviation. There is no rule of thumb though to determine when both of them will be same.",
+                                            textangle=0,
+                                            xanchor='left',
+                                            xref="paper",
+                                            yref="paper"))
     return(fig, data, columns)
 
-
-value = st.slider('SoH threshold', 100,1000,300)
-fig, data, columns = build_graph(value)
+st.title("Bessel's Correction - Lightning Talk at PyData Global 2022")
+st.header("Sample Size")
+sample_size = st.slider('Sample Size', 100,1000,300)
+st.header("Population Size")
+pop_size = st.slider('Population Size', 40000,80000,50000)
+fig, data, columns = bessel_correction(sample_size, pop_size)
+st.subheader("How Uncorrected vs Unbiased Standard Deviation varies with Sample and Population size")
 st.plotly_chart(fig, use_container_width=True)
 st.dataframe(data, 1200)
